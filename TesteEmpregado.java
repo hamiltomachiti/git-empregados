76f427e29ff40114f1ce21691f9890a4adc44792
Hamilton <prof.hamiltonmachiti@usjt.br>
2021-03-19 19:47:50 -0300

    Adiçao das lista na classe TesteEmpregado

--- TesteEmpregado.java
+++ TesteEmpregado.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class TesteEmpregado {
     public static void main(String[] args) {
         Empregado e1 = new Empregado();
@@ -17,6 +19,15 @@ public class TesteEmpregado {
         System.out.println(e1.calculaSalario());
         System.out.println(e2.calculaSalario());
         System.out.println(e3.calculaSalario());
+        // adicionando todo mundo em uma lista
+        List<Empregado> emps = Arrays.asList(e1, e2, e3);
+        // mostra os valores
+        /*
+         * System.out.println (e1.calculaSalario()); System.out.println
+         * (e2.calculaSalario()); System.out.println (e3.calculaSalario());
+         */
+        for (Empregado e : emps)
+            System.out.println(e.calculaSalario());
     }
 
 }
